edfed44f6f4c3c70b904e142c1d83ce77a01e0c8
Simone <morettinisimo96@gmail.com>
2021-03-09 22:56:05 +0100

    Added state machine

---
+++ StateMachine/stateMachineSteer.py
@@ -0,0 +1,88 @@
+from statemachine import StateMachine
+
+class StateMachineSteer:
+    '''
+    Implementation of the specific StateMachine following the need of our project
+    '''
+    memory=[]
+    m={}
+
+    def __init__(self):
+        self.m = StateMachine()
+        #Declaration of all the states
+        self.m.add_state("Steady", self.Steady_transitions)
+        self.m.add_state("OnLane", self.OnLane_transitions)
+        self.m.add_state("NearCrossroad", self.NearCrossroad_transitions)
+        self.m.add_state("OnCrossroad", self.OnCrossroad_transitions)
+        self.m.set_start("Steady")
+
+    #method for evaluating the OR of a list of variables
+    @staticmethod
+    def conditionOr(op,data, names, values):
+        result=False
+        for i,key in enumerate(names):
+            if(op==">"):
+                result=result or abs(data[key])>values[i]
+            elif(op == "=="):
+                result=result or abs(data[key])==values[i]
+            else:
+                result=result or abs(data[key])<values[i]
+        return result
+
+    #method for evaluating the AND of a list of variables
+    @staticmethod
+    def conditionAnd(op,data, names, values):
+        result=True
+        for i,key in enumerate(names):
+            if(op==">"):
+                result=result and abs(data[key])>values[i]
+            elif(op == "=="):
+                result=result and abs(data[key])==values[i]
+            else:
+                result=result and abs(data[key])<values[i]
+        return result
+
+    #Declaration of the transition from the Steady_transitions to the other possible states
+    def Steady_transitions(self, data):
+        if self.conditionAnd("==", data, ["moving"], [True]):
+            newState = "OnLane"
+        else:
+            newState = "Steady"
+        return newState
+
+    #Declaration of the transition from the StateB to the other possible states
+    def OnLane_transitions(self, data):
+        if self.conditionAnd("==", data, ["horizontal_line"], [True]):
+            newState = "NearCrossroad"
+        elif self.conditionAnd("==", data, ["moving"], [False]):
+            newState = "Steady"
+        else:
+            newState = "OnLane"
+        return newState
+
+    #Declaration of the transition from the StateC to the other possible states
+    def NearCrossroad_transitions(self, data):
+        if self.conditionAnd("==", data, ["horizontal_line"], [False]):
+            newState = "OnCrossroad"
+        else:
+            newState = "NearCrossroad"
+        return newState
+
+    #Declaration of the transition from the StateC to the other possible states
+    def OnCrossroad_transitions(self, data):
+        if self.conditionAnd("==", data, ["turning"], [False]):
+            newState = "OnLane"
+        else:
+            newState = "OnCrossroad"
+        return newState
+
+    def runOneStep(self,data):
+        return self.m.runOneStep(data)
+
+#Code for testing the state machine
+#
+#if __name__== "__main__":
+#    m = StateMachineSM()
+#
+#    m.runOneStep({"a" :0,"b" :0,"c":0, "d":0 })
+#    m.runOneStep({"a" :1,"v" :1,"c":1, "d":1 })
--- processer.py
+++ processer.py
@@ -30,7 +30,8 @@ from time import sleep
 import rospy
 
 from SimulatorCode.templates import Consumer
-
+from SimulatorCode.StateMachine.stateMachineSteer import StateMachineSteer
+from SimulatorCode.StateMachine.StateMachineVelocity import StateMachineVelocity
 
 
 class Processer(Consumer):
@@ -60,26 +61,73 @@ class Processer(Consumer):
 		self.subscribe("PidControlValues", "velocity_steer")
 		#rospy.Subscriber("HorizontalLine", String, self.HorizontalCheck)
 
+		state_machine=true
+
+		stateMachineSteer = StateMachineSteer()
+		stateMachineVelocity = StateMachineVelocity()
+
+		speed = 0.22
+		steering = 0
+		current_speed = 0.22   # For incrementing or decrementing to reach a target
 
 		while not rospy.is_shutdown():
-			#cv.imshow("Processer", cam.getImage())
-			#cv.waitKey(1)
-			speed = 0.18
-			steering = 0
-			self.car.drive(speed, steering)
-			sleep(0.1)
-			#print("Sending move with speed "+ str(speed) + " , steering " + str(steering))
-			if("street_lines" in self.data):
-				lines = json.loads(self.data["street_lines"])
-				#print(lines[0][0])
-			if("velocity_steer" in self.data):
-				velocity_steer = json.loads(self.data["velocity_steer"])
-				steering = -velocity_steer["steer"]
+			if(state_machine):
+				data_state_machine={}
+
+				data_state_machine["moving"] = True
+				data_state_machine["horizontal_line"] = True
+				data_state_machine["turning"] = True
+				data_state_machine["stop_signal"] = False
+				data_state_machine["pedestrian"] = True
+				data_state_machine["pedestrian_line"] = True
+				data_state_machine["state_steer"] = True
+
+				state_steer=stateMachineSteer.runOneStep(data_state_machine)
+				if(state_steer=="Steady"):
+					steering = 0
+				elif(state_steer=="OnLane"):
+					velocity_steer = json.loads(self.data["velocity_steer"])
+					steering = -velocity_steer["steer"]
+				elif(state_steer=="NearCrossroad"):
+					velocity_steer = json.loads(self.data["velocity_steer"])
+					steering = -velocity_steer["steer"]
+				elif(state_steer=="OnCrossroad"):
+					steering = turn_right()	# RIGHT
+					steering = 0			# STRAIGHT
+					steering = turn_left()	# LEFT
+
+				state_velocity=stateMachineVelocity.runOneStep(data_state_machine)
+				if(state_velocity=="OnSteady"):
+					speed = 0
+				elif(state_velocity=="Slow"):
+					if(current_speed > 0.2):
+						speed = min(0.2, current_speed-0.1)
+					else:
+						speed = max(0.2, current_speed+0.1)
+				elif(state_velocity=="Fast"):
+					if(current_speed > 0.8):
+						speed = min(0.8, current_speed-0.1)
+					else:
+						speed = max(0.8, current_speed+0.1)
+				current_speed = speed
+			else:
+				#cv.imshow("Processer", cam.getImage())
+				#cv.waitKey(1)
+
+				#print("Sending move with speed "+ str(speed) + " , steering " + str(steering))
+				if("street_lines" in self.data):
+					lines = json.loads(self.data["street_lines"])
+					#print(lines[0][0])
+				if("velocity_steer" in self.data):
+					velocity_steer = json.loads(self.data["velocity_steer"])
+					steering = -velocity_steer["steer"]
+				self.car.drive(speed, steering)
+
 			self.car.drive(speed, steering)
 			sleep(0.1)
 
 
-		rospy.spin()
+		#rospy.spin()
 
 	#def HorizontalCheck(self, data):
 	#	print("HorizontalCheck")
